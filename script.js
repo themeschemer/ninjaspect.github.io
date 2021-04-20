984cf9ff9fc42960d5bd1b4b569366ac7a79fe33
The Ninja <69609060+samuraininja360@users.noreply.github.com>
2021-04-20 21:19:43 +1200

    Updated JS

--- script.js
+++ script.js
@@ -3,6 +3,7 @@ var suBtn = document.getElementById("subtn")
 var closeModal = document.getElementById("close-modal")
 var usernameBox = document.getElementById("username-box")
 var passwordBox = document.getElementById("password-box")
+var submitForm = document.getElementById("submit-form")
 
 suBtn.addEventListener("click", function () {
   //signUp.style.display = "block"
@@ -38,8 +39,31 @@ function createRipple(event) {
   
     button.appendChild(circle)
 }
+
+function submitRipple(event) {
+  const button = event.currentTarget
+  
+    const circle = document.createElement("span")
+    const diameter = Math.max(button.clientWidth, button.clientHeight)
+    const radius = diameter / 2
+  
+    circle.style.width = circle.style.height = `${diameter}px`;
+    circle.style.left = `${event.pageX - button.offsetLeft - radius - window.innerWidth * 0.745}px`
+    circle.style.top = `${event.pageY - button.offsetTop - radius}px`
+    circle.classList.add("ripple")
+  
+    const ripple = button.getElementsByClassName("ripple")[0]
+  
+    if (ripple) {
+      ripple.remove()
+    }
+  
+    button.appendChild(circle)
+}
     
-const buttons = document.getElementsByTagName("button");
+const buttons = document.getElementsByClassName("ripplebtn");
 for (const button of buttons) {
     button.addEventListener("mousedown", createRipple);
 }
+
+submitForm.addEventListener("click", submitRipple)
